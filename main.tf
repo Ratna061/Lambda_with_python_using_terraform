ad7a2049fbb7f2a85e6f82504bafbd6587afd9bf
Ratna061 <160356197+Ratna061@users.noreply.github.com>
2024-02-18 13:16:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ EOF
 
 # IAM policy for logging from a lambda
 resource "aws_iam_policy" "iam_policy_for_lambda" {
-
   name        = "aws_iam_policy_for_terraform_aws_lambda_role"
   path        = "/"
   description = "AWS IAM Policy for managing aws lambda role"
